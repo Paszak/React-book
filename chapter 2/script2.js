8a2b6c79fa4064d7343f7c14703ceee70448035a
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-09-13 14:07:53 +0200

    chapter 2 component with state

--- chapter 2/script2.js
+++ chapter 2/script2.js
@@ -1,20 +1,37 @@
 var TextAreaCounter = React.createClass({
+	getInitialState: function() {
+		return {
+			text:this.props.text,
+		};
+	},
+
 	propTypes: {
 		text: React.PropTypes.string,
 	},
+
 	getDefaultProps: function() {
 		return {
 			text: '',
 		};
 	},
 	
+	handleChange: function(e) {
+		this.setState({
+			text: e.target.value,
+		});
+	},
+
 	render: function() {
-	return React.createElement('div', {}, 
-		React.createElement('textarea',{defaultValue: this.props.text}),
-		React.createElement('h3',{},this.props.text.length)
-		);
-	}
-});
+		return React.createElement('div', {}, 
+			React.createElement('textarea',
+				{
+					defaultValue: this.state.text,
+					onChange: this.handleChange,
+				}),
+			React.createElement('h3',{},this.state.text.length)
+			);
+		}
+	});
 
 var element = React.createElement(TextAreaCounter, {text:'paweł'});
 
